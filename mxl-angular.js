3d4710cdcbbc9993638d3a1ede87a443ea334c6f
treschenhofer <reschenhofert@lapmatthes6.sebis.ad.informatik.tu-muenchen.de>
2015-03-09 23:27:46 +0100

    implementation of validation, integration of lint, merge of ui-codemirror and mxl-angular

--- mxl-angular.js
+++ mxl-angular.js
@@ -1,5 +1,7 @@
-﻿angular.module('mxl', ['ui.codemirror'])
-.directive('mxlExpression', function ($timeout, uiCodemirrorConfig) {
+﻿'use strict';
+
+angular.module('mxl', ['ngDialog'])
+.directive('mxlExpression', function ($timeout, $q, ngDialog) {
     return {
         restrict: 'E',
         require: ["^ngModel"],
@@ -7,22 +9,104 @@
             {
                 expression: '=ngModel',
                 lineNumbers: '@mxlLinenumbers',
+                readOnly: '@mxlReadonly',
+                debounce: '@mxlDebounce',
                 additionalAutoCompletionHints: '=mxlAutocompletionhints',
                 runTest: '&mxlRuntest',
-                validate: '&mxlValidate'
+                validateMxl: '&mxlValidate'
             },
-        controller: function ($scope) {    
-            uiCodemirrorConfig.codemirror = {
+        link: function ($scope, $element, $attrs, ctrl) {
+           
+            function newCodemirrorEditor($element, codemirrorOptions) {
+                var codemirror;
+                $element.html('');
+                codemirror = new window.CodeMirror(function (cm_el) {
+                    $element.append(cm_el);
+                }, codemirrorOptions);
+                return codemirror;
+            }
+
+            function normalizeScopeValue() {
+                $scope.lineNumbers = $scope.$eval($scope.lineNumbers);
+                $scope.readOnly = $scope.$eval($scope.readOnly);
+                if(!$attrs.mxlValidate)
+                {
+                    $scope.validateMxl = null;
+                }
+                if (!$attrs.mxlRuntest || $scope.readOnly) {
+                    $scope.runTest = null;
+                }
+            }
+
+            function configNgModelLink(codemirror, ctrl, $scope) {
+
+                ctrl.$formatters.push(function (value) {
+                    if (angular.isUndefined(value) || value === null) {
+                        return '';
+                    }
+                    return value;
+                });
+
+                ctrl.$render = function () {
+                    codemirror.setValue(ctrl.$viewValue || '');
+                };
+
+                ctrl.$asyncValidators.typeChecking = function (modelValue, viewValue) {
+                    if (codemirror.options.validateMxl) {
+                        return codemirror.options.validateMxl(modelValue, viewValue);
+                    }
+                    return $q.when();
+                };
+
+                codemirror.on('change', function (instance) {
+                    var newValue = instance.getValue();
+                    if (newValue !== ctrl.$viewValue) {
+                        if (codemirror.options.debounce) {
+                            if ($scope.debounceUpdate) {
+                                $timeout.cancel($scope.debounceUpdate);
+                            }
+
+                            $scope.$evalAsync(function () {
+                                $scope.debounceUpdate = $timeout(function () {
+                                    ctrl.$setViewValue(newValue);
+                                }, codemirror.options.debounce);
+                            });
+                        }
+                        else {
+
+                            $scope.$evalAsync(function () {
+                                ctrl.$setViewValue(newValue);
+                            });
+                        }
+                    }
+                });
+
+                codemirror.on('blur', function (instance) {
+                    var newValue = instance.getValue();
+                    if (newValue !== ctrl.$viewValue) {
+                        $scope.$evalAsync(function () {
+                            ctrl.$setViewValue(newValue);
+                        });
+                    }
+                });
+            }
+
+            normalizeScopeValue();
+
+            var codemirrorOptions = {
                 lineWrapping: true,
                 matchBrackets: true,
                 autoCloseBrackets: true,
                 highlightSelectionMatches: { showToken: /\w/ },
                 viewportMargin: Infinity,
-                lineNumbers: $scope.lineNumbers && $scope.lineNumbers === "true",
+                lineNumbers: $scope.lineNumbers,
+                readOnly: $scope.readOnly,
                 mode: 'mxl',
-                debounce: 2000,
+                gutters: ["CodeMirror-lint-markers"],
+                lint: true,
+                debounce: $scope.debounce ? $scope.debounce : 2000,
                 theme: 'mxl',
-                extraKeys: angular.extend({
+                extraKeys: {
                     "Ctrl-Space": "autocomplete",
                     "Tab": false,
                     "Shift-Tab": false,
@@ -33,12 +117,49 @@
                     "esc": function (cm) {
                         return CodeMirror.Pass;
                     }
-                }, ($scope.runTest) ? {
-                    "F9": function (cm) {
-                        return $scope.runTest();
-                    }
-                } : {})
+                }
             };
+
+            if ($scope.validateMxl) {
+                codemirrorOptions.validateMxl = function (modelValue, viewValue) {
+                    function updateLints(currentError) {
+                        if ($scope.codemirror.options.updateMxLLints) {
+                            $scope.codemirror.options.updateMxLLints($scope.codemirror, currentError);
+                        }
+                    }
+
+                    return $q.when($scope.validateMxl({ modelValue: modelValue, viewValue: viewValue }))
+                        .then(function (response) {
+                            updateLints(null);
+                        }
+                            , function (response) {
+                                updateLints(response.data);
+                            });
+                };
+            }
+
+            if ($scope.runTest) {
+                codemirrorOptions.extraKeys["Ctrl-Enter"] = function (cm) {
+                    $q.when($scope.runTest({ value: cm.getValue() }))
+                        .then(function (response) {
+                            console.log(response);
+                            ngDialog.open({
+                                template: '<h4>Test result</h4>\
+                                            <p>' + response + '</p>\
+                                            <div class="ngdialog-buttons">\
+                                                <button type="button" class="ngdialog-button ngdialog-button-secondary" ng-click="closeThisDialog(0)">OK</button>\
+                                            </div>',
+                                plain: true,
+                                showClose: true,
+                                closeByDocument: true,
+                                closeByEscape: true
+                            });
+                        });
+                }
+            }
+            $scope.codemirror = newCodemirrorEditor($element, codemirrorOptions);
+
+            configNgModelLink($scope.codemirror, ctrl[0], $scope);
 
             CodeMirror.commands.autocomplete = function (cmeditor) {
                 var autoCompletionOptions = { completeSingle: false };
@@ -49,8 +170,6 @@
 
                 CodeMirror.showHint(cmeditor, CodeMirror.hint.mxl, autoCompletionOptions);
             };
-
-        },
-        templateUrl: 'mxl/mxl-template.html'
+        }
     }
 });
---
+++ mxl-lint.js
@@ -0,0 +1,42 @@
+﻿(function (mod) {
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+        mod(require("../../lib/codemirror"));
+    else if (typeof define == "function" && define.amd) // AMD
+        define(["../../lib/codemirror"], mod);
+    else // Plain browser env
+        mod(CodeMirror);
+})(function (CodeMirror) {
+    "use strict";
+
+    CodeMirror.registerHelper("lint", "mxl", function (text, passOptions, cm, currentError) {
+        var found = [];
+
+        if (currentError && currentError.statusCode >= 400 && currentError.cause.indexOf("MxL") == 0) {
+            var lastLine = cm.doc.lineCount() - 1;
+            var lastCol = cm.doc.getLine(lastLine).length - 1;
+            var range = currentError.additionalInformation ?
+                {
+                    fromLine: currentError.additionalInformation.from.line - 1,
+                    fromCol: currentError.additionalInformation.from.column - 1,
+                    toLine: currentError.additionalInformation.to.line - 1,
+                    toCol: currentError.additionalInformation.to.column
+                } :
+                {
+                    fromLine: lastLine,
+                    fromCol: lastCol,
+                    toLine: lastLine,
+                    toCol: lastCol
+                };
+
+            found.push({
+                from: CodeMirror.Pos(range.fromLine, range.fromCol),
+                to: CodeMirror.Pos(range.toLine, range.toCol),
+                message: currentError.message,
+                severity: "error"
+            });
+        }
+
+        return found;
+    });
+
+});
